bb790001656a37ddf7b494d5022040abae7ff410
Serafimova Ksenia <serafimova@rain.ifmo.ru>
2016-02-19 23:23:59 +0400

    Update main.c

--- task1/main.c
+++ task1/main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, const char *argv[])
 {
-    int n,i,j,X1,X2,Y1,Y2,space;
+    int n,i,j,X1,X2,Y1,Y2,b,space;
     const int b=5;
     scanf("%d", &n);
     int **a=(int**)malloc(n*sizeof(int*));
@@ -18,7 +18,12 @@ int main(int argc, const char *argv[])
     while (X1){
     if (X1 != 0){
             scanf("%d%d%d", &Y1,&X2,&Y2);
-            space = (int)log10(a[X2][Y2])+2;
+            b = a[X2][Y2];
+            space=1;
+            while b(>0){
+                b=b/10;
+                space=space+1;
+            }            
             for(i=X1-1;i<X2;i++){
                 for (j=Y1-1;j<Y2;j++){
                     printf("%-*d", space, a[i][j]);
