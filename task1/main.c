8d21fca9b25ee709d75fc5c7aefacc7897962aab
Serafimova Ksenia <serafimova@rain.ifmo.ru>
2016-02-20 14:21:12 +0400

    Update main.c

--- task1/main.c
+++ task1/main.c
@@ -1,1 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, const char *argv[])
+{
+    int n,i,j,X1,X2,Y1,Y2,space;
+    scanf("%d", &n);
+    int **a=(int**)malloc(n*sizeof(int*));
+    for (i=0;i<n;i++){
+        a[i]=(int*)malloc(n*sizeof(int));
+        for (j=0;j<n;j++){
+            a[i][j]=(i+1)*(j+1);
+        }
+    }
+   char b[20];
+   sprintf(b,"%d",a[n-1][n-1]);
+   space=strlen(b)+1;
+    scanf("%d",&X1);
+    while (X1){
+            scanf("%d%d%d", &Y1,&X2,&Y2); 
+            printf("\n");         
+            for(i=X1-1;i<X2;i++){
+                for (j=Y1-1;j<Y2;j++){
+                    printf("%*d", space, a[i][j]);
+                }
+                printf("\n");
+            }
+    scanf ("%d",&X1);
+    }
+    for (i=0;i<n;i++){
+        free(a[i]);
+    }
+    free(a);
+    return 0;
+}
 
