fa14b106735e067eacd9d91efb2f59a43860d874
Serafimova Ksenia <serafimova@rain.ifmo.ru>
2016-05-17 02:03:50 +0300

    Add files via upload

---
+++ lazy_string.cpp
@@ -0,0 +1,66 @@
+#include <stdexcept>
+#include "lazy_string.h"
+//#include <iostream>
+
+using namespace std;
+
+lazy_string::lazy_string() {
+    (*this).kawaii = string();
+    (*this).beginning = (*this).sz = 0;
+}
+
+lazy_string lazy_string::substr(size_t position, size_t size) {
+    if (position > sz)
+        throw std::out_of_range("lazy_string");
+    return lazy_string(
+            general,
+            position + size > sz ? (sz - position) : size,
+            beginning + position
+    );
+}
+
+const char& lazy_string::at(size_t position) const {
+    if (position>=sz)
+        throw std::out_of_range("lazy_string");
+    return (*this).kawaii[(*this).beginning + position];
+}
+
+const char& lazy_string::operator[](size_t position) const {
+    return at(position);
+}
+
+istream& operator>>(istream &input, lazy_string &lazy) {
+    input >> lazy.kawaii;
+    lazy.beginning = 0;
+    lazy.sz = lazy.kawaii.size();
+    return input;
+}
+
+ostream& operator<<(ostream &output, lazy_string &lazy) {
+    for(size_t x = lazy.beginning, i = 0; i < lazy.sz; x++, i++)
+        output << lazy[x];
+    return output;
+}
+
+size_t lazy_string::size() const {
+    return (*this).sz;
+}
+
+size_t lazy_string::length() const {
+    return (*this).sz;
+}
+
+lazy_string::lazy_string(const string &str) {
+    (*this).kawaii = str;
+    (*this).beginning = 0;
+    (*this).sz = str.size();
+}
+
+lazy_string::lazy_string(const lazy_string &lazy, size_t beginning, size_t size) {
+    (*this).kawaii = lazy.kawaii;
+    (*this).beginning = beginning;
+    (*this).sz = size;
+}
+
+
+
---
+++ lazy_string.h
@@ -0,0 +1,95 @@
+#ifndef LAZY_STRING_H
+#define	LAZY_STRING_H
+
+#include <string>
+#include <istream>
+#include <ostream>
+#include <bits/shared_ptr.h>
+
+using namespace std;
+
+
+class lazy_string {
+
+private:
+    size_t beginning, sz;
+    string kawaii;
+    lazy_string general;
+    lazy_string(const lazy_string &general, size_t beginning, size_t size);
+
+public:
+
+    /**
+     * Creates empty lazy_string (zero size and undefined capacity).
+     */
+    lazy_string();
+
+    /**
+     * Returns a newly constructed string object with its value initialized to a copy of a substring of this object.
+     * @param character's position
+     * @param number of characters
+     * @return the object that starts at character position pos and spans size characters
+     *         (or until the end of the string, whichever comes first).
+     */
+
+    lazy_string substr (size_t position = 0, size_t size = std::string::npos);
+
+    /**
+     * The function automatically checks whether position is the valid position of a character in the string
+     * (i.e., whether position is less than the string length), throwing an out_of_range exception if it is not.
+     * @param character's index.
+     * @return reference to the character at position pos in the string.
+     */
+    const char& at(size_t position) const;
+
+    /**
+    * Returns at(size_t position).
+    * @param character's position.
+    * @return character at specified position.
+    */
+    const char& operator[](size_t position) const;
+
+    /**
+    * Extracts a lazy_string from the input stream input, storing the sequence in lazy,
+    * which is overwritten (the previous value of ls is replaced).
+    * @param input stream.
+    * @param lazy_string.
+    * @return the same as parameter input.
+    */
+    friend istream& operator>>(istream& input, lazy_string& lazy);
+
+    /**
+    * Inserts the sequence of characters that conforms value of lazy into output.
+    * @param output stream.
+    * @param lazy_string.
+    * @return the same as parameter output.
+    */
+    friend ostream& operator<<(ostream& output, lazy_string& lazy);
+
+    /**
+     *  Returns the number of characters in the string.
+     * @return string's length (size).
+     */
+    size_t size() const;
+
+    /**
+     * Returns the number of characters in the string.
+     * @return string's length (size).
+     */
+    size_t length() const;
+
+    /**
+     * Creates new lazy_string from the given std::string.
+     * @return lazy_string instance.
+     */
+    lazy_string(const string &str);
+
+    /**
+     * Creates std::string using COW method from this lazy_string.
+     * @return std::string instance.
+     */
+    operator std::string();
+
+};
+
+#endif	/* LAZY_STRING_H */
